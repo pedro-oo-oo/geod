e249013aa00c724ab8c415ac4e08702b321f6cb7
pedro-oo-oo <pnapieraj2@gmail.com>
2025-05-21 15:43:21 +0200

    Documentation added and cleanup
    Cleaned up the mess in geod.grav,
    rewritten the funcs in a more godly way.
    Added documentation to most funcs, should be easier to use and understand.

--- grav.py
+++ grav.py
@@ -25,91 +25,221 @@ def dg_height(height):
 def drift():
     ...
     
-def gamma0_grs80(phi):
+def gamma0_grs80(phi: geod.Angle) -> float:
     gamma_0 = (978032.66 
                * (1 + 0.0053024*phi.sin()**2 - 0.0000058*phi.sin(2)**2))
     return (gamma_0)
 
-def dg_bouguer(height, density=2.67):
+def dg_bouguer(height: float, density: float = 2.67) -> float:
     """
     Returns dg_bouguer correction to gravimetry calculations.
     Unit: mGal
     """
     return round(-0.04192 * density * height, 4)
 
-def dg_poincaryprey(height, density=2.67):
+def dg_poincaryprey(height: float, density: float = 2.67) -> float:
+    """
+    Returns dg_poincaryprey correction to gravimetry calculations.
+    Unit: mGal
+    """
     return round((0.3086 - 2*0.04192 * density) * height, 4)
 
-def anomaly(phi, g_measured, dg):
-    # g0 = gamma0_grs80(phi)
-    delta = g_measured + dg - gamma0_grs80(phi)
+def anomaly(phi: geod.Angle, g: float, dg: float) -> float:
+    """
+    Returns gravitational anomaly in a given point, in mGal
+
+    Args:
+        phi (geod.Angle):
+            Point's latitude
+        g (float):
+            Gravitational acceleration in mGal
+        dg (float): 
+            dg correction (Height / Bouguer / Poincary-Prey)
+
+    Returns:
+        float: DESCRIPTION.
+
+    """
+    
+    delta = g + dg - gamma0_grs80(phi)
     return round(delta, 4)
     
-def height_dif_geopot(h_dif, g_a, g_b):
+def height_dif_geopot(h_dif: float, g_a:float, g_b: float) -> float:
+    """
+    Adjusts the measured height difference between 2 points to the 
+    geopotential height system.
+
+    Args:
+        h_dif (float): 
+            Measured height difference between points
+        g_a (float): 
+            Gravitational acceleration measured on starting point in mGal
+        g_b (float):
+            Gravitational acceleration measured on ending point in mGal
+
+    Returns:
+        Height difference in geopotential system.
+    """    
     gi = np.average([g_a, g_b])
-    return (gi - 1e6)/1e6 * h_dif
+    return h_dif + (gi - 1e6)/1e6 * h_dif
     
-def height_dif_dynamic(h_dif, g_a, g_b):
+def height_dif_dynamic(h_dif: float, g_a:float, g_b: float) -> float:
+    """
+    Adjusts the measured height difference between 2 points to the 
+    dynamic height system.
+
+    Args:
+        h_dif (float): 
+            Measured height difference between points
+        g_a (float): 
+            Gravitational acceleration measured on starting point in mGal
+        g_b (float):
+            Gravitational acceleration measured on ending point in mGal
+
+    Returns:
+        Height difference in dynamic system.
+    """
     gi = np.average([g_a, g_b])
     g45 = gamma0_grs80(geod.Angle([45]))
-    return (gi - g45)/g45 * h_dif
-    # assert len(h_difs) == len(g_args), 'h_difs must be of 1 size smaller than g_args'
+    return h_dif + (gi - g45)/g45 * h_dif    
     
-    
-def height_dif_orthometric(h_a, h_b, dhi, g_a, g_b):
-    # assert len(h_difs) == len(g_args), 'h_difs must be of 1 size smaller than g_args'
+def height_dif_orthometric(H_appr_A: float,
+                           H_appr_B: float,
+                           h_dif: float,
+                           g_a: float,
+                           g_b: float) -> float:
+    """
+    Adjusts the measured height difference between 2 points to the 
+    orthometric height system.
+
+    Args:
+        H_appr_A (float):
+            Approximate (or exact if there) height of starting point 
+        H_appr_B (float):
+            Approximate (or exact if there) height of ending point 
+        h_dif (float): 
+            Measured height difference between points
+        g_a (float): 
+            Gravitational acceleration measured on starting point in mGal
+        g_b (float):
+            Gravitational acceleration measured on ending point in mGal
+
+    Returns:
+        Height difference in orthometric system.
+    """
     g045 = gamma0_grs80(geod.Angle(np.pi/4))
-    gr_a = g_a + dg_height(h_a/2) + 2*dg_bouguer(h_a/2)
-    gr_b = g_b + dg_height(h_b/2) + 2*dg_bouguer(h_b/2)
+    gr_a = g_a + dg_height(H_appr_A/2) + 2*dg_bouguer(H_appr_A/2)
+    gr_b = g_b + dg_height(H_appr_B/2) + 2*dg_bouguer(H_appr_B/2)
     gi = np.average([g_a, g_b])
-    return ((gi-g045) / g045 * dhi
-            + (gr_a - g045) / g045 * h_a
-            - (gr_b - g045) / g045 * h_b)
+    return ((gi-g045) / g045 * h_dif
+            + (gr_a - g045) / g045 * H_appr_A
+            - (gr_b - g045) / g045 * H_appr_B) + h_dif
     
-    
-    # ga = g[0] + dg_poincaryprey(h_a/2) 
-    # ga = (ga - gamma0_grs80(obj.Angle(np.pi/4))) / gamma0_grs80(obj.Angle(np.pi/4))
-    # gb = g_args[-1] + dg_poincaryprey(h_b/2) 
-    # gb = (gb - gamma0_grs80(obj.Angle(np.pi/4))) / gamma0_grs80(obj.Angle(np.pi/4))
-    
-    # try:    
-    #     return sum(((g_args[i]-g45) / (g45) * h_difs[i]) for i in range(len(h_difs))) + ga - gb
-    # except TypeError :
-    #     return (g_args[0] - g45) / (g45) * h_difs + ga - gb 
-    
-def height_dif_normal(phi_a, h_a, phi_b, h_b, dhi, g_a, g_b):
-    # # assert len(h_difs) == len(g_args), 'h_difs must be of 1 size smaller than g_args'
-    # g45 = gamma0_grs80(obj.Angle(np.pi/4))
-    # ga = gamma0_grs80(phi_a) - dg_height(h_a/2) 
-    # ga = (ga - gamma0_grs80(obj.Angle(np.pi/4))) / gamma0_grs80(obj.Angle(np.pi/4))
-    # gb = gamma0_grs80(phi_b) - dg_height(h_b/2)
-    # gb = (gb - gamma0_grs80(obj.Angle(np.pi/4))) / gamma0_grs80(obj.Angle(np.pi/4))
-    # try:    
-    #     return sum(((g_args[i]-g45) / (g45) * h_difs[i]) for i in range(len(h_difs))) + ga - gb
-    # except TypeError:
-    #     return (g_args - g45) / (g45) * h_difs + ga - gb 
+def height_dif_normal(phi_a: geod.Angle, phi_b: geod.Angle,
+                      H_appr_A: float, H_appr_B: float, 
+                      h_dif: float, 
+                      g_a: float, g_b: float) -> float:
+    """
+    Adjusts the measured height difference between 2 points to the 
+    normal height system.
+
+    Args:
+        phi_a (geod.Angle):
+            Latitude of starting point
+        phi_b (geod.Angle):
+            Latitude of ending point
+        H_appr_A (float):
+            Approximate (or exact if there) height of starting point 
+        H_appr_B (float):
+            Approximate (or exact if there) height of ending point 
+        h_dif (float): 
+            Measured height difference between points
+        g_a (float): 
+            Gravitational acceleration measured on starting point in mGal
+        g_b (float):
+            Gravitational acceleration measured on ending point in mGal
+
+    Returns:
+        Height difference in normal system.
+    """
     
     gi = np.average([g_a, g_b])
     g045 = gamma0_grs80(geod.Angle([np.pi/4]))
-    gam_a = gamma0_grs80(phi_a) - 0.3086*(h_a/2)
-    gam_b = gamma0_grs80(phi_b) - 0.3086*(h_b/2)
-    return ((gi-g045) / g045 * dhi
-            + (gam_a - g045) / g045 * h_a
-            - (gam_b - g045) / g045 * h_b)
+    gam_a = gamma0_grs80(phi_a) - 0.3086*(H_appr_A/2)
+    gam_b = gamma0_grs80(phi_b) - 0.3086*(H_appr_B/2)
+    return ((gi-g045) / g045 * h_dif
+            + (gam_a - g045) / g045 * H_appr_A
+            - (gam_b - g045) / g045 * H_appr_B) + h_dif
     
-def height_geopot(Cp):
-    return Cp / 10**6
+def height_geopot(C):
+    """
+    Returns height in geopotential system.
 
-def height_dynamic(Cp):
-    return Cp / gamma0_grs80(geod.Angle(np.pi/4))
+    Args:
+        C (float | geod.grav.GeopotNum):
+            Geopotential number.
 
-def height_orthometric(Cp, g_measured, h):
-    g = g_measured + dg_poincaryprey(h/2) 
-    return Cp / g
+    Returns:
+        Height in geopotential system.
 
-def height_normal(Cp, phi, h):
-    g = gamma0_grs80(phi) - dg_height(h/2) 
-    return Cp / g
+    """
+    if hasattr(C, 'C'): C = C.C
+    return C / 10**6
+
+def height_dynamic(C):
+    """
+    Returns height in dynamic system.
+
+    Args:
+        C (float | geod.grav.GeopotNum):
+            Geopotential number.
+
+    Returns:
+        Height in dynamic system.
+
+    """
+    if hasattr(C, 'C'): C = C.C
+    return C / gamma0_grs80(geod.Angle(np.pi/4))
+
+def height_orthometric(C, g, H_appr):
+    """
+    Returns height in geopotential system.
+
+    Args:
+        C (float | geod.grav.GeopotNum):
+            Geopotential number.
+        g (float):
+            Gravitational acceleration in mGal
+        H_appr (float):
+            Approximate height of the point
+
+    Returns:
+        Height in geopotential system.
+
+    """
+    if hasattr(C, 'C'): C = C.C
+    g = g + dg_poincaryprey(H_appr/2) 
+    return C / g
+
+def height_normal(C, phi, H_appr):
+    """
+    Returns height in geopotential system.
+
+    Args:
+        C (float | geod.grav.GeopotNum):
+            Geopotential number.
+        phi (geod.Angle):
+            Latitude of the point
+        H_appr (float):
+            Approximate height of the point
+
+    Returns:
+        Height in geopotential system.
+
+    """
+    if hasattr(C, 'C'): C = C.C
+    g = gamma0_grs80(phi) - dg_height(H_appr/2) 
+    return C / g
 
 
 class Height:
@@ -133,7 +263,7 @@ class Height:
             Latitude of the point where Height is calculated            
         g (float): 
             Gravitational acceleration, in units of mGal.
-        h_appr (float): 
+        H_appr (float): 
             Approximate height of the point, 
             needed for gravity stuff calculations, i don even know bruh
     """
@@ -142,14 +272,14 @@ class Height:
                  output_type: str | None,
                  phi: geod.Angle | None = None, 
                  g: float | None = None,
-                 h_appr: float | None = None) -> None:
+                 H_appr: float | None = None) -> None:
         if hasattr(C, 'C'):
             self.C = C.C 
         else:
             self.C = C
         
         self.phi = phi
-        self.h_appr = h_appr
+        self.H_appr = H_appr
         self.g = g
         
         self.output_type = output_type
@@ -159,16 +289,16 @@ class Height:
         match output_type:
             case 'orthometric':
                 assert self.g is not None, 'Unspecified g in Height initialization'
-                assert self.h_appr is not None, 'Unspecified h_appr in Height initialization'
-                return height_orthometric(self.C, self.g, self.h_appr)
+                assert self.H_appr is not None, 'Unspecified H_appr in Height initialization'
+                return height_orthometric(self.C, self.g, self.H_appr)
             
             case 'dynamic':
                 return height_dynamic(self.C)
             
             case 'normal':
                 assert self.phi is not None, 'Unspecified phi in Height initialization'
-                assert self.h_appr is not None, 'Unspecified h_appr in Height initialization'
-                return height_normal(self.C, self.phi, self.h_appr)
+                assert self.H_appr is not None, 'Unspecified H_appr in Height initialization'
+                return height_normal(self.C, self.phi, self.H_appr)
                 
             case 'geopotential':
                 return height_geopot(self.C)
@@ -181,6 +311,28 @@ class Height:
     
 
 class GeopotNum:
+    """
+    Calculate a point's geopotential number
+    without dealing with all pesky stuff.
+
+    Args:
+        height (float): 
+            Point's height in m
+        system (str): 
+            Select the height system in which the height is given.
+            'normal / orthometric / dynamic / geopotential'   
+            
+        These next ones are not mandatory, used only for certain systems,
+        you'll be warned if you miss them dw pookie
+        
+        phi (geod.Angle): 
+            Latitude of the point where GeopotNum is calculated            
+        g (float): 
+            Gravitational acceleration, in units of mGal.
+        density (float):
+            Land mass density below the point, 
+            defaults to 2.67g/cm3 being the avg density of Earth
+    """
     def __init__(self, height: float,
                  system: str,
                  phi: geod.Angle | None = None, 
@@ -207,5 +359,8 @@ class GeopotNum:
                 raise TypeError('Invalid height system, read documentation dude')
                 
     def __str__(self):
+        'print me daddy uwu'
         return f'{self.C:.3f}'
+    
+
                 
